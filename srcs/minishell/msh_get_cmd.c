d791da60adc8655fd2f14f9915ca174012631396
cfrancoi <cfrancoi@student.le-101.fr>
2020-11-02 16:00:48 +0100

    merge"

--- srcs/minishell/msh_get_cmd.c
+++ srcs/minishell/msh_get_cmd.c
@@ -6,14 +6,21 @@
 /*   By: cfrancoi <cfrancoi@student.le-101.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 15:29:50 by cfrancoi          #+#    #+#             */
-/*   Updated: 2020/11/02 15:31:16 by cfrancoi         ###   ########lyon.fr   */
+/*   Updated: 2020/11/02 15:58:10 by cfrancoi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/msh_get_cmd.h"
 
+
+
 char		**msh_get_cmd(char *ptr)
 {
+	t_arg	*lst;
+	
+	lst = NULL;
+
+	get_arg_lst();
 
 	return (NULL);
 }
