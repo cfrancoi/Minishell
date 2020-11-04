d2a8877a4d29f842b57c4a3b01289227666516a3
lulu4747 <ludogriph@gmail.com>
2020-11-04 17:16:29 +0100

    echo + blank builtins

--- srcs/builtins/echo/echo.c
+++ srcs/builtins/echo/echo.c
@@ -1,26 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/02 14:26:16 by cfrancoi          #+#    #+#             */
-/*   Updated: 2020/11/04 15:25:08 by user42           ###   ########.fr       */
+/*   Created: 2020/11/04 15:47:39 by user42            #+#    #+#             */
+/*   Updated: 2020/11/04 16:50:28 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef		MINISHELL_H
-# define	MINISHELL_H
+# include "../../include/builtins.h"
 
-# include	<unistd.h>
-# include	"../libft/libft.h"
-# include	"./env.h"
-# include	"./msh_parsing.h"
-# include	"./cfrancoi.h"
-# include	"./lfourage.h"
+int	main(int ac, char **av)
+{
+	int	i;
+	int n;
 
-
-int			ft_prompt(void);
-
-# endif
+	i = 0;
+	n = 0;
+	if (ac == 1)
+		return (0);
+	while (++i < ac)
+	{
+		while (ft_strncmp(av[i], "-n", 3) == 0)
+		{
+			n = 1;
+			i++;
+		}
+		ft_putstr_fd(1, av[i])
+		if (i + 1 != ac)
+			ft_putchar_fd(1, ' ');
+	}
+	if (n == 0)
+		ft_putchar_fd(1, '\n');
+	return (0);
+}
--- srcs/include/builtins.h
+++ srcs/include/builtins.h
@@ -1,26 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/02 14:26:16 by cfrancoi          #+#    #+#             */
-/*   Updated: 2020/11/04 15:25:08 by user42           ###   ########.fr       */
+/*   Created: 2020/11/04 15:12:33 by user42            #+#    #+#             */
+/*   Updated: 2020/11/04 15:13:31 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef		MINISHELL_H
-# define	MINISHELL_H
+#ifndef ENV_H
+# define ENV_H
 
-# include	<unistd.h>
-# include	"../libft/libft.h"
-# include	"./env.h"
-# include	"./msh_parsing.h"
-# include	"./cfrancoi.h"
-# include	"./lfourage.h"
-
-
-int			ft_prompt(void);
-
-# endif
+# include "minishell.h"
