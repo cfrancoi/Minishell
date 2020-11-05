96a07f0ee931884552bdd784b69599184c65b6cd
lulu4747 <ludogriph@gmail.com>
2020-11-05 16:28:41 +0100

    exit 2

--- srcs/minishell/msh_exit.c
+++ srcs/minishell/msh_exit.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 15:57:17 by user42            #+#    #+#             */
-/*   Updated: 2020/11/05 16:16:50 by user42           ###   ########.fr       */
+/*   Updated: 2020/11/05 16:27:40 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void		msh_exit(t_cmd *cmd, t_arg *arg, int ret)
 		if (cmd->str)
 			free(cmd->str);
 		ft_array_free(cmd->av);
-		(t_cmd)tofree = cmd;
+		(t_cmd *)tofree = cmd;
 		cmd = cmd->next;
 		free(tofree);
 	}
@@ -29,9 +29,9 @@ void		msh_exit(t_cmd *cmd, t_arg *arg, int ret)
 	{
 		if (arg->ptr)
 			free(arg->ptr);
-		(t_arg)tofree = arg;
+		(t_arg *)tofree = arg;
 		arg = arg->next;
-		free(to_free);
+		free(tofree);
 	}
 	exit (ret);
 }
