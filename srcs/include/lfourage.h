c95f12f4e0523a1a176eb77cdfe8eddd9a9c8965
lulu4747 <ludogriph@gmail.com>
2020-11-05 16:18:05 +0100

    exit

--- srcs/include/lfourage.h
+++ srcs/include/lfourage.h
@@ -3,4 +3,6 @@
 
 # include	"./minishell.h"
 
+void	msh_exit(t_cmd *cmd, t_arg *arg, int ret);
+
 # endif
---
+++ srcs/minishell/msh_exit.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   msh_exit.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/05 15:57:17 by user42            #+#    #+#             */
+/*   Updated: 2020/11/05 16:16:50 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+void		msh_exit(t_cmd *cmd, t_arg *arg, int ret)
+{
+	void	*tofree;
+
+	while (cmd)
+	{
+		if (cmd->str)
+			free(cmd->str);
+		ft_array_free(cmd->av);
+		(t_cmd)tofree = cmd;
+		cmd = cmd->next;
+		free(tofree);
+	}
+	while (arg)
+	{
+		if (arg->ptr)
+			free(arg->ptr);
+		(t_arg)tofree = arg;
+		arg = arg->next;
+		free(to_free);
+	}
+	exit (ret);
+}
