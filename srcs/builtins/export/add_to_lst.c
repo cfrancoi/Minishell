21c1eacae48200d3bf6fed4df7dea38e65e35e1d
cfrancoi <cfrancoi@student.le-101.fr>
2020-11-20 17:26:31 +0100

    export error + patch

--- srcs/builtins/export/add_to_lst.c
+++ srcs/builtins/export/add_to_lst.c
@@ -6,7 +6,7 @@
 /*   By: cfrancoi <cfrancoi@student.le-101.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 16:17:40 by cfrancoi          #+#    #+#             */
-/*   Updated: 2020/11/20 15:45:59 by cfrancoi         ###   ########lyon.fr   */
+/*   Updated: 2020/11/20 17:25:48 by cfrancoi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static int			is_name_var(char *str)
 	i = 0;
 	while(str[i] && str[i] != '=' && (ft_isalnum(str[i]) || str[i] == '_'))
 		i++;
-	if (str[i] == '=')
+	if (str[i] == '=' || str[i] == '\0')
 		return (1);
 	else
 		return (0);
@@ -33,23 +33,6 @@ static t_var			*is_existing_var(char *name)
 
 	return(tmp);
 }
-
-/*
-static t_var		*to_add(char *ptr)
-{
-	int i;
-	t_var	**lst;
-
-	lst = &g_all.var;
-	i = 0;
-	while (ptr[i + 1] && ptr[i] != '=')
-		i++;
-	ptr[i] = '\0';
-	t_var *tmp = create_var(&ptr[0], &ptr[i + 1]);
-	print_var(tmp);
-	add_var(lst, tmp);
-	return (tmp);
-}*/
 
 static int		to_add(char *ptr)
 {
@@ -86,7 +69,7 @@ static int		to_add(char *ptr)
 /* En cas d'erreur un message est send sans interompre la fct */
 /* 
 	Exemple : export OUI!=NON NON=OUI 
-	on aura un message d'erreur mais non sera cree
+	on aura un message d'erreur pour $OUI mais $NON sera cree
 */
 
 #include <stdio.h>
@@ -94,14 +77,21 @@ static int		to_add(char *ptr)
 int		add_to_lst(int ac, char **av) 
 {
 	int i;
+	int ret;
 
+	ret = 0;
 	i = 1;
 	while (i < ac)
 	{
 		if (to_add(av[i]) != 0)
-			printf("export : error << %s >>\n", av[i]);
+		{
+			ret = 1;
+			ft_putstr_fd("msh : export : << ", 2);
+			ft_putstr_fd(av[i], 2);
+			ft_putstr_fd(" >> : identifiant non valable\n", 2);
+		}
 		i++;
 	}
-	return (0);	
+	return (ret);	
 }
 
