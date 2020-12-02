ba31accdbfd8402cccc49c5681f89bbd5cc7642f
cfrancoi <cfrancoi@student.le-101.fr>
2020-12-02 14:47:49 +0100

    truc casser

--- srcs/minishell/msh_sig.c
+++ srcs/minishell/msh_sig.c
@@ -6,7 +6,7 @@
 /*   By: cfrancoi <cfrancoi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 16:22:11 by cfrancoi          #+#    #+#             */
-/*   Updated: 2020/12/01 16:35:14 by cfrancoi         ###   ########lyon.fr   */
+/*   Updated: 2020/12/01 18:03:47 by cfrancoi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,32 @@
 static void sigint_handler(int signum)
 {
     (void)signum;
-    ft_putstr_fd("crt-c\n", 1);
     return ;
+}
+
+static void sigquit_handler(int signum)
+{
+    (void)signum;
+
+    ft_putstr_fd("crtl-\\\n", 1);
+    return ;
+}
+
+static void sigkill_handler(int signum)
+{
+    (void)signum;
+
+    ft_putstr_fd("crtl-d\n", 1);
+    exit(-1);
 }
 
 int     msh_sig()
 {
     if ((signal(SIGINT, sigint_handler)) == SIG_ERR)
+        return (-1);
+    if ((signal(SIGQUIT, sigquit_handler)) == SIG_ERR)
+        return (-1);
+    if ((signal(15, sigkill_handler)) == SIG_ERR)
         return (-1);
     return (0);
 }
